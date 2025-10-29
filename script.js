9b6e5059085478c9ddbe35c963a8e32508c7917f
recruitmentbaoxiansen <recruitmentbaoxiansen@gmail.com>
2025-10-29 18:40:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const daysTag = document.querySelector(".days"),
-currentDate = document.querySelector(".current-date"),
-prevNextIcon = document.querySelectorAll(".month-nav span");
+      currentDate = document.querySelector(".current-date"),
+      prevNextIcon = document.querySelectorAll(".month-nav span");
 
 let date = new Date(),
-currYear = date.getFullYear(),
-currMonth = date.getMonth();
+    currYear = date.getFullYear(),
+    currMonth = date.getMonth();
 
 const months = ["January", "February", "March", "April", "May", "June",
-              "July", "August", "September", "October", "November", "December"];
+                "July", "August", "September", "October", "November", "December"];
 
 const renderCalendar = () => {
   let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
-  lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
-  lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(),
-  lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+      lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
+      lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
   
   let liTag = "";
 
@@ -42,15 +41,4 @@ const renderCalendar = () => {
 renderCalendar();
 
 prevNextIcon.forEach(icon => {
-  icon.addEventListener("click", () => {
-    currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
-    if (currMonth < 0 || currMonth > 11) {
-      date = new Date(currYear, currMonth, new Date().getDate());
-      currYear = date.getFullYear();
-      currMonth = date.getMonth();
-    } else {
-      date = new Date();
-    }
-    renderCalendar();
-  });
-});
+  icon.addEventList
